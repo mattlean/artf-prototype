3fe35ac436922dfe77952e5ea3b0a48492267b64
IsaacLean <isaaclean@creatibyte.com>
2014-11-19 08:35:12 -0800

    Added game over screen and music

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -2,22 +2,25 @@
 
 public class GameOverManager : MonoBehaviour
 {
-    public PlayerHealth playerHealth;
+    public PlayerHealth playerHealth; //Reference to the player's health
 
 
-    Animator anim;
+    Animator anim; //Reference to the animator component
 
 
     void Awake()
     {
+		//Setup the reference
         anim = GetComponent<Animator>();
     }
 
 
     void Update()
     {
+		//If the player has run out of health...
         if (playerHealth.currentHealth <= 0)
         {
+			//...tell the animator the game is over.
             anim.SetTrigger("GameOver");
         }
     }
