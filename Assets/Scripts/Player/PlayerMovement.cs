8ea5def590c99569cb351f950a0938cc05c3a5ab
IsaacLean <isaaclean@creatibyte.com>
2014-11-20 02:29:00 -0800

    Got proper basic directional movement control working

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
 	Animator anim; //Reference to the animator's component
 	Rigidbody playerRigidbody; //Reference to the player's rigidbody
 	int floorMask; //A layer mask so that a ray can be cast just a game objs on the floor layer
-	float camRayLength = 100f; //The length of the ray from the camera into the scene
+	Vector3 playerDirection = Vector3.forward; //Determines the direction the player is facing
 
 	void Awake() {
 		//Create a layer mask for the floor layer
@@ -21,14 +21,11 @@ public class PlayerMovement : MonoBehaviour
 
 	void FixedUpdate() {
 		//Store the input axes
-		float h = Input.GetAxisRaw ("Horizontal");
-		float v = Input.GetAxisRaw ("Vertical");
+		int h = (int)Input.GetAxisRaw ("Horizontal");
+		int v = (int)Input.GetAxisRaw ("Vertical");
 
 		//Move the player around the scene
 		Move (h, v);
-
-		//Turn the player to face the mouse cursor
-		Turning ();
 
 		//Animate the player
 		Animating (h, v);
@@ -43,29 +40,31 @@ public class PlayerMovement : MonoBehaviour
 
 		//Move the player to its current position plus the movement
 		playerRigidbody.MovePosition (transform.position + movement);
-	}
-
-	void Turning() {
-		//Create a ray from the mouse cursor on screen in the direction of the camera
-		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-
-		//Create a RaycastHit variable to store information about what was hit by the ray
-		RaycastHit floorHit;
-
-		//Perform the raycast and if it hits something on the floor layer...
-		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
-			//Create a vector from the player to the point on the floor the raycast from the mouse hit
-			Vector3 playerToMouse = floorHit.point - transform.position;
-
-			//Ensure the vector is entirely along the floor plane
-			playerToMouse.y = 0f;
-
-			//Create a quaternion (rotation) based on looking down the vector from the player to the mouse
-			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
-
-			//Set the player's rotation to this new rotation
-			playerRigidbody.MoveRotation (newRotation);
+		
+		if (h == -1) {
+			playerDirection = Vector3.left;
+		} else if (h == 1) {
+			playerDirection = Vector3.right;
 		}
+
+		if (v == -1) {
+			playerDirection = Vector3.back;
+		} else if (v == 1) {
+			playerDirection = Vector3.forward;
+		}
+
+		if (h == -1 && v == -1) {
+			playerDirection = new Vector3(-1, 0, -1);
+		} else if (h == 1 && v == -1) {
+			playerDirection = new Vector3(1, 0, -1);
+		} else if (h == -1 && v == 1) {
+			playerDirection = new Vector3(-1, 0, 1);
+		} else if (h == 1 && v == 1) {
+			playerDirection = new Vector3(1, 0, 1);
+		}
+
+		Quaternion newDirection = Quaternion.LookRotation(playerDirection, Vector3.up);
+		playerRigidbody.MoveRotation (newDirection);
 	}
 
 	void Animating(float h, float v) {
