1f980c3342db8dee0a1affb369690b7beb69a0ee
IsaacLean <isaaclean@creatibyte.com>
2014-12-04 08:39:49 -0800

    Got basic psynergy reticle movement working

--- Assets/Scripts/Player/Player1Movement.cs
+++ Assets/Scripts/Player/Player1Movement.cs
@@ -7,16 +7,20 @@ public class Player1Movement : MonoBehaviour {
 	Rigidbody playerRigidbody; //Reference to the player's rigidbody
 	Animator anim; //Reference to the animator's component
 
+	public GameObject reticle;
+	private Transform reticleLocation;
+	bool isCasting = false;
+
 	void Awake() {
 		//Setup references
 		playerRigidbody = GetComponent<Rigidbody> ();
 		anim = GetComponent<Animator> ();
+
+		Instantiate (reticle, new Vector3(transform.position.x, -1f, transform.position.y), Quaternion.Euler(new Vector3(90, 0, 0)));
+		reticleLocation = GameObject.Find ("P1Reticle(Clone)").GetComponent<Transform>();
 	}
 
-	void Move() {
-		float h = Input.GetAxisRaw ("Horizontal");
-		float v = Input.GetAxisRaw ("Vertical");
-
+	void Move(float h, float v) {
 		//control running animation
 		bool walking = h != 0f || v != 0f;
 		anim.SetBool ("IsWalking", walking);
@@ -100,10 +104,54 @@ public class Player1Movement : MonoBehaviour {
 		}
 	}
 
+	void PsynergyActivate() {
+		if (Input.GetButtonDown ("Fire2")) {
+			isCasting = true;
+			anim.SetBool ("IsWalking", false);
+			reticleLocation.position = new Vector3(transform.position.x, 3f, transform.position.z);
+		}
+	}
+
+	void PsynergyMove(float h, float v) {
+		float offsetX = 0;
+		float offsetZ = 0;
+		float moveSpeed = 0.03f;
+
+		//determine movement
+		if (h == -1) {
+			offsetX = -moveSpeed;
+		} else if (h == 1) {
+			offsetX = moveSpeed;
+		}
+		
+		if (v == -1) {
+			offsetZ = -moveSpeed;
+		} else if (v == 1) {
+			offsetZ = moveSpeed;
+		}
+
+		reticleLocation.position = new Vector3 (reticleLocation.position.x + offsetX, reticleLocation.position.y, reticleLocation.position.z + offsetZ);
+	}
+
+	void PsynergyRelease() {
+		if (Input.GetButtonDown ("Fire2")) {
+			isCasting = false;
+			reticleLocation.position = new Vector3(0, -1f, 0);
+		}
+	}
+
 	// Update is called once per frame
 	void Update() {
-		Move ();
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
 
-		Attack ();
+		if (isCasting) {
+			PsynergyMove (h, v);
+			PsynergyRelease ();
+		} else {
+			Move (h, v);
+			Attack ();
+			PsynergyActivate ();
+		}
 	}
 }
